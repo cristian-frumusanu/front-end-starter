e6f565525aea5c5675f41fef150fb33dbc744bcc
Cristian Frumusanu <cristian.frumusanu@gmail.com>
2018-07-25 12:42:49 +0200

    Updated package and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,69 @@
-var gulp = require( 'gulp-help' )( require( 'gulp' ) );
+var gulp = require('gulp');
 
 // Gulp / Node utilities
-var u = require( 'gulp-util' );
-var log = u.log;
-var c = u.colors;
-var exec = require( 'gulp-exec' );
-var del = require( 'del' );
-var fs = require( 'fs' );
-var rename = require( 'gulp-rename' );
-var replace = require( 'gulp-replace' );
-var prompt = require( 'gulp-prompt' );
+var log = require('fancy-log');
+var c = require('ansi-colors');
 
 // Basic workflow plugins
-var prefix = require( 'gulp-autoprefixer' );
-var sass = require( 'gulp-sass' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-var concat = require( 'gulp-concat' );
-var csscomb = require( 'gulp-csscomb' );
-var cmq = require( 'gulp-combine-mq' );
-var sequence = require( 'gulp-sequence' );
+var prefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 var jsFiles = [
-	'./assets/js/main/wrapper-start.js',
-	'./assets/js/main/shared-vars.js',
-	'./assets/js/modules/*.js',
-	'./assets/js/main/main.js',
-	'./assets/js/main/wrapper-end.js',
-	'./assets/js/plugins/*.js'
+	'./assets/js-dev/main/wrapper-start.js',
+	'./assets/js-dev/main/shared-vars.js',
+	'./assets/js-dev/modules/*.js',
+	'./assets/js-dev/main/main.js',
+	'./assets/js-dev/main/wrapper-end.js',
+	'./assets/js-dev/plugins/*.js'
 ];
 
+// -----------------------------------------------------------------------------
+// Sass Task
+//
+// Compiles Sass and runs the CSS through autoprefixer. A separate task will
+// combine the compiled CSS with vendor files and minify the aggregate.
+// -----------------------------------------------------------------------------
 
-function logError( err, res ) {
-	log( c.red( 'Sass failed to compile' ) );
-	log( c.red( '> ' ) + err.file.split( '/' )[err.file.split( '/' ).length - 1] + ' ' + c.underline( 'line ' + err.line ) + ': ' + err.message );
+function logError(err, res) {
+	log(c.red('Sass failed to compile'));
+	log(c.red('> ') + err.file.split('/')[err.file.split('/').length - 1] + ' ' + c.underline('line ' + err.line) + ': ' + err.message);
 }
 
+gulp.task('styles', function styles() {
+	return gulp.src('assets/scss/*.scss')
+		.pipe(sass({ style: 'compressed' }).on('error', logError))
+		.pipe(prefix())
+		.pipe(gulp.dest('assets/css'));
+});
 
-// ------------------------
-// Combine SCSS files
-// ------------------------
-gulp.task( 'styles', 'Compiles main css files (ie. style.css editor-style.css)', function() {
-
-	return gulp.src( 'assets/scss/*.scss' )
-	           .pipe( sass({ style: 'compressed' }).on( 'error', logError ) )
-	           .pipe( prefix() )
-	           .pipe( gulp.dest( 'assets/css' ) );
-} );
-
-function logError (error) {
-	console.log(error.toString());
-	this.emit('end');
-}
-
-// ------------------------
+// -----------------------------------------------------------------------------
 // Combine JavaScript files
-// ------------------------
-gulp.task( 'scripts', 'Concatenate all JS into main.js and wrap all code in a closure', function() {
-	return gulp.src( jsFiles )
+// -----------------------------------------------------------------------------
+gulp.task('scripts', function scripts() {
+	return gulp.src(jsFiles)
 		// Concatenate all our files into main.js
-		.pipe( concat( 'main.js' ) )
-		.pipe( gulp.dest( './assets/js/' ) );
-} );
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('server', gulp.parallel('styles', 'scripts'), function () {
+	console.log('The styles and scripts have been compiled for production! Go and clear the caches!');
+});
 
 
-gulp.task( 'server', 'Compile scripts and styles for production purposes', ['styles', 'scripts'], function() {
-	console.log( 'The styles and scripts have been compiled for production! Go and clear the caches!' );
-} );
-
-
-gulp.task( 'watch', 'Watch for changes to various files and process them', function() {
-	gulp.watch( 'assets/scss/**/*.scss', ['styles'] );
-	gulp.watch( 'assets/js/**/*.js', ['scripts'] );
-} );
+// -----------------------------------------------------------------------------
+// Watch tasks
+//
+// These tasks are run whenever a file is saved. Don't confuse the files being
+// watched (gulp.watch blobs in this task) with the files actually operated on
+// by the gulp.src blobs in each individual task.
+//
+// A few of the performance-related tasks are excluded because they can take a
+// bit of time to run and don't need to happen on every file change. If you want
+// to run those tasks more frequently, set up a new watch task here.
+// -----------------------------------------------------------------------------
+gulp.task('watch', function watch() {
+	gulp.watch('assets/scss/**/*.scss', gulp.series('styles'));
+	gulp.watch('assets/js-dev/**/*.js', gulp.series('scripts'));
+});
